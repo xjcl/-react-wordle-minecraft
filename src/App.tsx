94b3f91d3493062c31ed29d394d39a6f0b7771eb
Jan Konopka <janfrederik.konopka@gmail.com>
2022-04-02 12:47:39 +0200

    refactor: simplify prefersDarkMode check

--- src/App.tsx
+++ src/App.tsx
@@ -58,8 +58,6 @@ function App() {
     localStorage.getItem('theme')
       ? localStorage.getItem('theme') === 'dark'
       : prefersDarkMode
-      ? true
-      : false
   )
   const [isHighContrastMode, setIsHighContrastMode] = useState(
     getStoredIsHighContrastMode()
