f35f0583be184afe4809e7b5b51cfc9c92190755
Jan Konopka <janfrederik.konopka@gmail.com>
2022-04-03 21:32:07 +0200

    feat: write user howto for game

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,63 +10,38 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        I'm thinking of a Minecraft crafting recipe, and you have 3 guesses (of
+        actual recipes) to get it.
+        <br/><br/>
+        After each guess, the color of the tiles
+        changes to show how close your guess was to the word.
       </p>
 
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="W"
-          status="correct"
-        />
-        <Cell value="E" />
-        <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
+      <div className="mt-4 mb-2">
+        <div className="flex justify-center mb-1">
+            <Cell value="I" status="correct"/>
+            <Cell value=" " status="present"/>
+            <Cell value="I" status="present"/>
+        </div>
+        <div className="flex justify-center mb-1 ">
+          <Cell value="I" status="correct"/>
+          <Cell value="S" status="correct"/>
+          <Cell value="I" status="absent"/>
+        </div>
+        <div className="flex justify-center mb-1 ">
+          <Cell value="I" status="absent"/>
+          <Cell value=" " status="present"/>
+          <Cell value="I" status="absent"/>
+        </div>
       </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="L"
-          status="present"
-        />
-        <Cell value="O" />
-        <Cell value="T" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
-        <Cell value="A" />
-        <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
-        <Cell value="E" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
-      </p>
-
-      <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
-        <a
-          href="https://github.com/cwackerfuss/react-wordle"
-          className="underline font-bold"
-        >
-          check out the code here
-        </a>{' '}
-      </p>
+    <p className="text-sm text-gray-500 dark:text-gray-300">
+        <i>Green: This block is in the right spot.<br/>
+        Yellow: Block is in the recipe but wrong spot.<br/>
+        Gray: No (more) copies of block in the recipe.</i>
+        <br/><br/>
+        Items smaller than 3x3 are centered, otherwise in the bottom-left (except for helmets, they are on top).
+        Tools always use iron.
+    </p>
     </BaseModal>
   )
 }
